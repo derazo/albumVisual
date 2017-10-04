013690d32d561cc55ac05014964c9ecf70829fed
derazo <haerazo@umariana.edu.co>
2017-10-04 21:32:28 +0200

    Commit inicial

---
+++ album/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from album.models import Category, Photo
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Photo)
